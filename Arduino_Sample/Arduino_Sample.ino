05ce8514dc6fcb803fcf9d0c20daca9e0a600360
UEFI-code <zhuoshizhang@hotmail.com>
2025-02-08 16:11:50 +0900

    Add Arduino Sensor Demo

---
+++ Arduino_Sample/Arduino_Sample.ino
@@ -0,0 +1,26 @@
+#define threshold 512
+
+void setup()
+{
+    // set A0 as input
+    pinMode(A0, INPUT);
+    Serial.begin(9600);
+}
+
+int sensorValue = 0;
+char StatusBuffer[16] = {'I', 'D', 'L', 'E', ' ', 0};
+
+void loop()
+{
+    sensorValue = analogRead(A0);
+    if (sensorValue > threshold)
+    {
+        Serial.print("ALERT");
+    }
+    else
+    {
+        itoa(sensorValue, StatusBuffer+5, 10);
+        Serial.print(StatusBuffer);
+    }
+    delay(1000);
+}
